431644d15ba861fdc430ce8e44d3c32911db7631
pandeylab <pandeylab@s45-40-133-8.secureserver.net>
2018-05-05 04:10:47 -0700

    Version as on 15-Apr-18

---
+++ careers.php
@@ -0,0 +1,224 @@
+<html>
+<head>
+<link rel="shortcut icon" href="images/iob.ico" type="image/x-icon" />
+<title>Institute of Bioinformatics</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="description" content="The Institute of Bioinformatics (IOB) established by Dr. Akhilesh Pandey, is a non-profit academic research organization located at International Tech Park in Bangalore, India. IOB is recognized by the Department of Scientific and Industrial Research, Government of India." /> 
+<meta name="keywords" content="bioinformatics, india, bangalore, database, institute, proteomics, mass spectrometry, mass spectrometer, LTQ-Orbitrap, proteomics bangalore, india, genomics, protein reference database, oncomine, HPRD, akhilesh, akhilesh pandey, hprd, plasma proteome database, netpath, pathways, signaling pathways, pathbuilder, biobuilder, proteinpedia, human proteinpedia, SVM, RAPID, primary Immunodeficiency disease, cancer, biomarkers, biomarker, gstric cancer, pancreatic cancer, esophageal cancer, ESCC, X chromosome, XLMR, microarray, CGH array, neurological disorders, epilepsy, rabies, CNV, protein tyrosine phosphatases, gene expression arrays, tiling arrays, tissue microarrays, cell pellet arrays, breast cancer database, BCD, oncomine, phosphoproteomics, SILAC, iTRAQ, Glycoproteomics, proteogenomics, secretome, neuroproteomics, toxicoproteomics, plant proteomics, ethanoproteomics, serum proteomics, quantitative proteomics, HPLC, OFFGEL, nano-LC, ETD, Electron Transfer Dissociation, mycobacterium tuberculosis, research, proteomcis research, biomarker discovery, arsenic, Ph.D., manipal, kuvempu, pondicherry, amrita, rajiv gandhi university of health science, KIDWAI, NIMHANS, Johns Hopkins University, JHMI, RIKEN, RCAI, University of Toronto, IGIB, IICB, NIMR, MSKCC, sloan-kettering, University of Southern Denmark, HUPO, comparative proteomics, embryonic stem cells, anopheles, iob, comparative genomics, est">
+<link rel="stylesheet" href="css/style.css" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script type="text/javascript" src="jquery.aw-showcase.js"></script>
+<script type="text/javascript">
+
+$(document).ready(function()
+{
+	$("#showcase").awShowcase(
+	{
+	content_width:			540,
+		content_height:			236,
+		fit_to_parent:			false,
+		auto:					true,
+		interval:				3000,
+		continuous:				false,
+		loading:				true,
+		tooltip_width:			200,
+		tooltip_icon_width:		32,
+		tooltip_icon_height:	32,
+		tooltip_offsetx:		18,
+		tooltip_offsety:		0,
+		arrows:					true,
+		buttons:				true,
+		btn_numbers:			true,
+		keybord_keys:			true,
+		mousetrace:				false, /* Trace x and y coordinates for the mouse */
+		pauseonover:			true,
+		stoponclick:			false,
+		transition:				'hslide', /* hslide/vslide/fade */
+		transition_delay:		0,
+		transition_speed:		500,
+		show_caption:			'onload', /* onload/onhover/show */
+		thumbnails:				false,
+		thumbnails_position:	'outside-last', /* outside-last/outside-first/inside-last/inside-first */
+		thumbnails_direction:	'vertical', /* vertical/horizontal */
+		thumbnails_slidex:		1, /* 0 = auto / 1 = slide one thumbnail / 2 = slide two thumbnails / etc. */
+		dynamic_height:			false, /* For dynamic height to work in webkit you need to set the width and height of images in the source. Usually works to only set the dimension of the first slide in the showcase. */
+		speed_change:			true, /* Set to true to prevent users from swithing more then one slide at once. */
+		viewline:				false, /* If set to true content_width, thumbnails, transition and dynamic_height will be disabled. As for dynamic height you need to set the width and height of images in the source. */
+		custom_function:		null /* Define a custom function that runs on content change */
+	});
+});
+
+</script>
+<script language="JavaScript">
+<!--
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+//-->
+</script>
+
+<link href="style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+<!--
+.style3 {color: #FF0099}
+.style4 {color: #FF0066}
+.style5 {font-weight: bold; color: #FF33CC;}
+-->
+</style>
+</head>
+<body bgcolor="#E6E6E6" text="#000000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" onLoad="MM_preloadImages('images/collagiate/iob_roll.gif','images/buttons/csa_roll.gif','images/buttons/director_roll.gif','images/buttons/board_roll.gif','images/buttons/links_roll.gif','images/collagiate/team_roll.gif')">
+
+
+<table width="801" border="0" align="center" cellpadding="0" cellspacing="0">
+	<tr> 
+		<td width="801"  bgcolor="#FFFFFF" align="center"><?php require("header.htm");?> &nbsp;</td>
+	</tr> 
+	<tr>
+		<td>
+			<table width="803" height="781" bgcolor="#FFFFFF">
+				<tr>
+					<td width="88" height="728"  valign="top"><?php include('menu.html');?> </td>
+					<td width="703" valign="top" align="left">
+						<fieldset>
+							<table width="100%" height="455" border="0" cellpadding="0" cellspacing="0">
+			 					<tr>
+									<td colspan="3"   style="border-bottom:#0066FF medium groove; font:Geneva, Arial, Helvetica, sans-serif; font-size:14px" align="center"><strong>CAREERS</strong></td>
+    
+  </tr>
+      <tr>
+        <td height="18">&nbsp;</td>
+        <td width="19%" align="center">&nbsp;</td>
+        <td width="78%">&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="0" height="26">&nbsp;</td>
+        <td colspan="2" valign="top"><!--<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tr>
+              <td height="141" colspan="2" align="center" valign="top"><img src="images/careers.jpg" width="150" height="128" style="border-top: 4px solid white;">&nbsp;&nbsp;</td>
+              <td valign="top"><p align="justify">Institute of Bioinformatics
+                  (IOB) is a premier research center for Genomics, Proteomics,
+                  Metabolomics, and Computational biology. IOB has established
+                  a state-of-the-art proteomics, genomics, and metabolomics platforms
+                  that are being utilized to explore and validate candidate biomarkers
+                  in a wide array of human diseases ranging from neurological
+                  disorders and infections to cancers. Scientists at IOB are
+                  training young researchers across India in these OMICS technologies.
+                  IOB Researchers in collaboration with Johns Hopkins University
+                  and NIMHANS, have established a draft map of human proteome
+                  for the first time in the world. This landmark study was featured
+                  on the cover of the prestigious journal Nature on May 29, 2014.</p>
+                <div align="justify"><br>
+                    IOB is looking for highly motivated research students, who
+                          would like to investigate on altered proteomic and
+                      metabolomic profiles of human beings during diverse
+      disease conditions. A number of opportunities are available to carry out
+                        deeper proteomic and metabolomic profiles of specialized
+                        organs including brain and
+      eye. Only candidates who qualified Junior Research Fellowship and would
+                      like to pursue their Ph.D. degree in the cutting &#8211;edge
+                      research areas using advanced experimental platforms are
+                      encouraged to apply.</div></td>
+              <td width="2%">&nbsp;</td>
+            </tr>
+            <tr>
+              <td colspan="2">&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+          </table>-->
+
+<?php $Count=0; ?>
+<!-- <img src="images/JRF_flyer_new.gif" width="650" height="1020"> -->
+
+<p><b><?php echo ++$Count; ?>. Ph.D. Studentships in Computational Biology</b></p>
+<p>
+Applications are invited from talented and passionate candidates to work on several next generation sequencing (NGS)-based research studies as well as proteomic studies and pursue their Ph.D. in Computational Biology at the Institute of Bioinformatics (IOB) in Bangalore (http://www.ibioinformatics.org). The selected candidates will be enrolled for their Ph.D. at Manipal Academy of Higher Education (MAHE), Manipal. The thrust areas of IOB include infectious diseases, neurological disorders, cancer biology and computational biology. The selected candidates will undergo excellent training in next generation sequencing methods as well as Big Data analysis with opportunity to work on data generated from next generation sequencing methods such as whole genome/exome sequencing, transcriptome sequencing and small RNA sequencing. IOB is also internationally known for mass spectrometry-based proteomics, which provides opportunities to work on multi-omics studies such as proteogenomics, metabolomics, pathways and interaction networks.  </p>
+
+
+<p><b>Prerequisites:</b></p>
+<p>The applicants MUST HAVE:</p>
+<p>
+	<ol>
+		<li> M.Sc/M.Tech in Bioinformatics/ Biotechnology</li>
+		<li> Experience in coding with Python and R</li>
+		<li> Experience in PHP or other web technologies and MySQL</li>
+		<li> Experience working in a Linux environment and be able to do shell scripting</li>
+		<li> Fair knowledge of statistics</li>
+		<li> Experience in NGS data analysis</li>
+	</ol>
+	</p>
+	<p>Candidates who has secured fellowships from the State/Central Government agencies such as BINC, CSIR-UGC, DBT and DST-INSPIRE will be preferred. </p>
+	<p>Send your CVs to: careers [at] ibioinformatics [dot] org and write &quot;Ph.D.
+	  Studentships in Computational Biology&quot; in the Subject field of your e-mail.</p>
+	  
+    <p>Last date for applications is May 15, 2018. </p>
+<!--
+<p><b>Qualifications for SRF (5 positions):</b></p>
+<p>
+	<ol>
+		<li> M.Sc. or M.Tech in any field of Life Sciences</li>
+		<li> 2 years research experience and minimum 1 publication in any peer reviewed journal</li>
+	</ol>
+</p>
+
+<p>
+Selected JRFs and SRFs will be extensively trained in sample prep and data analysis of proteomic and multi PTM experiments on Plasmodium falciparum and its interaction with human, high-resolution mass spectrometry, maintenance of HPLC and culturing of synchronized culture of Plasmodium falciparum and a number of molecular biology techniques. Fellowship amounts will be as per DBT rules and guidelines. Interested candidates needs to send their detailed CV by email to: keshav@ibioinformatics.org with a subject line either as “JRF in Malaria Research” or “SRF in Malaria Research”. Shortlisted candidates will be contacted within 15 days from date of application. 
+</p>
+<div style="clear:both;float:none;padding:5px;"></div>
+
+
+
+<p><b>. Ph.D. studentship in Human Genomics (2 positions)</b></p>
+<p><b>Qualifications:</b></p>
+<p>
+	<ol>
+		<li> M.Sc. in any field of Life Sciences or Bioinformatics with experience in molecular biology techniques or an understanding of building, using bioinformatics tools, databases and basis of the underlying algorithms</li>
+		<li> Candidates must have secured JRF funding from CSIR-UGC, DBT, ICMR and DST-INSPIRE</li>
+	</ol>
+</p>
+
+<p>
+Selected JRFs will be involved in ongoing collaborative research projects between IOB and other collaborating institutes. They will be trained in cutting edge technologies including wet lab training for confirmation of NGS single nucleotide variants and splice mutation at the RNA level. In selected cases to participate in functional confirmation. Training will also be given on interpreting and curating NGS. Only candidates who can relocate to Bangalore (ITPL) need to apply. Interested candidates should e-mail their detailed curriculum vitae to <a href="mailto:careers@ibioinformatics.org"><strong>careers at ibioinformatics dot org</strong></a> with subject as "Ph.D. studentship in Human Genomics". Short listed candidates will be called for an interview. Those without any fellowship please do not apply.&#8203;
+</p>
+<div style="clear:both;float:none;padding:5px;"></div>
+
+<p><b> Ph.D. studentship in Cancer Biology (3 positions)</b></p>
+<p>
+Ph.D. admissions in Cancer Biology (3 positions) at Institute of Bioinformatics (IOB), Bangalore, which integrates multi-disciplinary expertise on biomarker research. The program involves the application of state of the art technologies to investigate biomarkers for bladder cancer recurrence and progression. This includes: identifying novel biomarkers, study the molecular pathology and understanding clinical phenotypes.
+</p>
+<p><b>Qualifications:</b></p>
+<p>
+	<ol>
+		<li> M.Sc. in Life Sciences</li>
+		<li> Candidates must have secured JRF funding from CSIR-UGC, DBT and BINC</li>
+	</ol>
+</p>
+
+<p>
+Candidates with experience in cell culture, molecular biology and/or mass spectrometry are preferred. Selected JRFs will be involved in cell culture, molecular biology experiments, proteomics and cutting edge technologies including NextGen sequencing. Only candidates who can relocate to Bangalore may apply. Interested candidates should e-mail their detailed curriculum vitae to <a href="mailto:careers@ibioinformatics.org"><strong>careers at ibioinformatics dot org</strong></a>  with subject as "Ph.D. studentship - Cancer Biology". Short listed candidates will be called for an interview. Those without any fellowship please do not apply.
+</p>
+<div style="clear:both;float:none;padding:5px;"></div>
+
+<p><b> Academic/Dissertation projects for M.Sc./M.Tech and B.Tech students</b></p>
+<p>
+IOB is a premier center for Proteomics and Computation Biology. IOB emphasizes cutting edge research in Databases, Computational Genomics, Proteomics and Comparative Genomics. IOB has developed systems biology resources of international repute including Human Protein Reference Database (published in <em>NAR</em> and <em>Genome Research</em>; cited by over 2000 research articles), Plasma Proteome Database (published in <em>Proteomics</em>), Human Proteinpedia (published in <em>Nature Biotechnology</em>), a resource for human signaling pathways called NetPath (published in <em>Genome Biology</em>), Pancreatic Cancer Biomarker Resource (published in <em>PLos Medicine</em>) and Resource for Asian Primary Immune Diseases (published in <em>NAR</em>). Besides developing resources, scientists at IOB performed a comparative genomic analysis of mouse and human X chromosomes and identified novel protein coding regions on the human X chromosome, which was published in <em>Nature Genetics</em>. Another study, also published in <em>Nature Genetics</em>, involved construction of largest interactome based on the protein-protein interaction data available in various protein databases.&nbsp;
+</p>
+
+<p>
+IOB is offering to extend its expertise in proteomics and development of signaling pathway maps to M.Sc./M.Tech/B.Tech students of various disciplines of Life Sciences in India in the form of Academic/dissertation projects for 4-6 months. Selected students will be trained in i) annotation of signaling pathways leading to individual manuscripts; ii) processing of clinical samples for proteomic analysis, and iii) analysis of mass spectrometry derived data. We will also train each research trainee on the preparation of manuscripts.
+<br>
+
+<p>
+A bench fee of Rs.50,000 will have to paid as DD in favor of &ldquo;INSTITUTE OF BIOINFORMATICS&rdquo;payable at Bangalore<br><br>
+Interested students should fill an online application form available at <a href="http://www.ibioinformatics.org/application.php" target="_blank">http://www.ibioinformatics.org/application.php</a>
+</p> -->
+
+<div style="clear:both;float:none;padding:5px;"></div>
+
+
+</body>
+</html>
